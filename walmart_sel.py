6c024d01ffe5096449b77a8cfb249e2a4c1245a3
Stephen Atehe <atehestephen@gmail.com>
2022-05-17 20:26:10 +0100

    clear failed script

--- walmart_sel.py
+++ walmart_sel.py
@@ -11,6 +11,7 @@ from selenium.webdriver.chrome.service import Service
 import logging
 import csv
 import time
+import random
 
 
 def click(element, driver):
@@ -32,10 +33,56 @@ def click(element, driver):
 # )
 # driver = webdriver.Chrome(options=option)
 import undetected_chromedriver as uc
+from scrapy.selector import Selector
+from csv import writer
 
 driver = uc.Chrome(version_main=100)
 driver.maximize_window()
-driver.get("https://www.walmart.com/all-departments")
+# driver.get("https://www.walmart.com/all-departments")
+driver.get(
+    "https://www.walmart.com/browse/home-improvement/dehumidifiers/1072864_133032_1231459_112918?povid=HardlinesGlobalNav_DSK_HomeImprovement_AirQualityWaterPurification_Dehumidifiers&affinityOverride=default"
+)
+time.sleep(random.randint(2, 4))
+
+with open("walmart.csv", "a") as csv_file:
+    csv_writer = writer(csv_file)
+
+    page_response = Selector(text=driver.page_source.encode("utf8"))
+    items = []
+
+    num_pages = page_response.xpath(
+        "//nav[@aria-label='pagination']//li[last()-1]//text()"
+    ).get()
+    num_pages = int(num_pages)
+    print(num_pages)
+    page = 2
+    while page <= num_pages:
+        page_response = Selector(text=driver.page_source.encode("utf8"))
+        products = page_response.xpath(
+            "//div[@class='mb1 ph1 pa0-xl bb b--near-white w-25']"
+        )
+        for product in products:
+            id = product.xpath("./div/div/@data-item-id").get()
+            url = product.xpath("./div/div/a/@href").get()
+            name = product.xpath("./div/div/a//text()").get()
+            price = product.xpath("./div/div/div/div[2]/div[1]//text()").get()
+            rating = product.xpath("./div/div/div/div[2]/div[2]/span[3]//text()").get()
+            print((name, id, url, price, rating))
+            csv_writer.writerow((name, id, url, price, rating))
+            # try:
+            #     next_page = driver.find_element(
+            #         by=By.XPATH, value="//a[@aria-label='Next Page']"
+            #     )
+            #     action = ActionChains(driver)
+            #     action.move_to_element(to_element=next_page)
+            #     action.click()
+            #     action.perform()
+            # except:
+            #     break
+        driver.get(
+            f"https://www.walmart.com/browse/home-improvement/dehumidifiers/1072864_133032_1231459_112918?povid=HardlinesGlobalNav_DSK_HomeImprovement_AirQualityWaterPurification_Dehumidifiers&affinityOverride=default&page={page}"
+        )
+        page += 1
 
 
 # department_button = WebDriverWait(driver, 30).until(
@@ -43,15 +90,15 @@ driver.get("https://www.walmart.com/all-departments")
 # )
 # click(department_button, driver)
 
-sections = driver.find_elements(
-    by=By.XPATH, value='//div[@class="w_Cf w_De w_Cr w_Cx w_C7 tl mt4"]'
-)
+# sections = driver.find_elements(
+#     by=By.XPATH, value='//div[@class="w_Cf w_De w_Cr w_Cx w_C7 tl mt4"]'
+# )
 
-for section in sections:
-    category = section.find_element(by=By.XPATH, value=".//h2/a").text
-    print(category)
-    subcategory_elements = section.find_elements(by=By.XPATH, value=".//ul/li/a")
-    for subcategory_element in subcategory_elements:
-        subcategory = subcategory_element.text
-        subcategory_url = subcategory_element.get_attribute("href")
-        print(subcategory, subcategory_url)
+# for section in sections:
+#     category = section.find_element(by=By.XPATH, value=".//h2/a").text
+#     print(category)
+#     subcategory_elements = section.find_elements(by=By.XPATH, value=".//ul/li/a")
+#     for subcategory_element in subcategory_elements:
+#         subcategory = subcategory_element.text
+#         subcategory_url = subcategory_element.get_attribute("href")
+#         print(subcategory, subcategory_url)
